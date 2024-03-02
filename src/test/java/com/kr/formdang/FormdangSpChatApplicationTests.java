e380eef8d380461a99eb626de32b4e7705a5cdc3
chzh859@naver.com <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>
2024-03-02 17:10:26 +0900

    Initial commit

---
+++ src/test/java/com/kr/formdang/FormdangSpChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.formdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormdangSpChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
